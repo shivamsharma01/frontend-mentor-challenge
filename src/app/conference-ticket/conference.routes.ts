dc5f67847ebaa19ffc1332a946efad5639ad27eb
shivam <shivamsharma.ade@gmail.com>
2025-01-03 22:34:24 +0530

    reusable components for submit and preview

--- src/app/conference-ticket/conference.routes.ts
+++ src/app/conference-ticket/conference.routes.ts
@@ -1,9 +1,26 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
+import { TicketPreviewComponent } from './ticket-preview/ticket-preview.component';
+import { TicketSubmitComponent } from './ticket-submit/ticket-submit.component';
 
 export const CONFERENCER_ROUTES: Routes = [
   {
     path: '',
     component: MainPageComponent,
+    children: [
+      {
+        path: 'submit',
+        component: TicketSubmitComponent,
+      },
+      {
+        path: 'preview',
+        component: TicketPreviewComponent,
+      },
+      {
+        path: '',
+        redirectTo: 'submit',
+        pathMatch: 'full',
+      },
+    ],
   },
 ];
