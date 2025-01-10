8fb0fb7bda0b3aa9fc8843dbe5e018c111b73d8a
shivam <shivamsharma.ade@gmail.com>
2025-01-10 13:27:17 +0530

    pass route data and display data

--- src/app/conference-ticket/ticket-preview/ticket-preview.component.ts
+++ src/app/conference-ticket/ticket-preview/ticket-preview.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-ticket-preview',
@@ -7,7 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./ticket-preview.component.css', '../conference-ticket.css'],
 })
 export class TicketPreviewComponent {
+  route = inject(ActivatedRoute);
+  location = inject(Location);
   name: string = 'Jonatan Kristof';
   email: string = 'Jonatan@gmail.com';
   githubId: string = 'jonatankristof0101';
+  image: string = '/assets/conference-ticket-generator/images/image-avatar.jpg';
+  
+  ngOnInit() {
+    const navigation = this.location.getState() as any;
+    if (navigation) {
+      this.name = navigation['name'];
+      this.githubId = navigation['id'];
+      this.email = navigation['email'];
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.image = reader.result as string;
+      };
+      reader.readAsDataURL(navigation['image']);
+    }
+  }
 }
