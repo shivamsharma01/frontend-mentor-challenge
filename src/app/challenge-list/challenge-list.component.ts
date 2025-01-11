03594ef9cd6acf0482a1a5cd9b87c1ee152d57cb
shivam <shivamsharma.ade@gmail.com>
2025-01-11 13:36:56 +0530

    add conference ticket challenge

---
+++ src/app/challenge-list/challenge-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-challenge-list',
+  imports: [RouterLink],
+  templateUrl: './challenge-list.component.html',
+  styleUrl: './challenge-list.component.css'
+})
+export class ChallengeListComponent {
+
+}
